b630ce1044a3f2039165f39ec2302115a1356ec7
Davide -limaCAT- Inglima <limacat@live.it>
2017-07-15 00:30:58 +0200

    - rimosso vecchio tentativo di refactoring - aggiunto quit

--- SoftTracer/Renderers/Raytracer.cpp
+++ SoftTracer/Renderers/Raytracer.cpp
@@ -73,12 +73,12 @@ namespace JBRaytracer {
 		const int thread_no = my_parameters->threadNumber;
 		const int m_Width = my_parameters->width;
 		const int m_Height = my_parameters->height;
-
+		
 		float m_WX1, m_WY1, m_WX2, m_WY2, m_DX, m_DY, m_SX, m_SY;
 		// set firts line to draw to
-		m_CurrLine = 20;
+		int m_CurrLine = 20;
 		// set pixel buffer address of first pixel
-		m_PPos = 20 * m_Width;
+		int m_PPos = 20 * m_Width;
 		// screen plane in world space coordinates
 		m_WX1 = -4, m_WX2 = 4, m_WY1 = m_SY = 3, m_WY2 = -3;
 		// calculate deltas for interpolation
@@ -96,7 +96,8 @@ namespace JBRaytracer {
 		// initialize timer
 		Primitive* lastprim = 0;
 		// render remaining lines
-		for (int y = m_CurrLine; y < (m_Height - 20); y++)
+		bool quit = false;
+		for (int y = m_CurrLine; !quit && y < (m_Height - 20); y++)
 		{
 			m_SX = m_WX1;
 			// render pixels for current line
@@ -119,6 +120,7 @@ namespace JBRaytracer {
 				m_SX += m_DX;
 			}
 			m_SY += m_DY;
+			quit = my_mailbox->to_children_quit;
 		}
 		// all done
 
@@ -367,153 +369,3 @@ namespace JBRaytracer {
 	}
 
 }; // namespace JBRaytracer
-
-namespace Attempt1 {
-
-JBEngine::JBEngine() {
-#ifdef _DEBUG
-	std::cout << "construtor called\n";
-#endif
-}
-
-JBEngine::~JBEngine() {
-#ifdef _DEBUG
-	std::cout << "destructor called\n";
-#endif
-	delete my_scene;
-}
-
-void JBEngine::initialize_scene(const void* parameters) {
-	const Parameters* my_parameters = (const Parameters *)parameters;
-	width = my_parameters->width;
-	height = my_parameters->height;
-
-
-	my_scene = new Scene();
-	my_scene->initialize_scene();
-}
-
-color calculateDiffuse(color start, float diffuse, color first, color second) {
-	std::vector<float> a_start = Array(start);
-	std::vector<float> a_first = Array(first);
-	std::vector<float> a_second = Array(second);
-	float r = a_start[0] + diffuse * a_first[0] * a_second[0];
-	float g = a_start[1] + diffuse * a_first[1] * a_second[1];
-	float b = a_start[2] + diffuse * a_first[2] * a_second[2];
-
-	return Color(r, g, b);
-
-}
-
-// -----------------------------------------------------------
-// Engine::Raytrace
-// Naive ray tracing: Intersects the ray with every primitive
-// in the scene to determine the closest intersection
-// -----------------------------------------------------------
-color JBEngine::Raytrace(Ray& a_Ray, int a_Depth, float a_RIndex, float& a_Dist) {
-	color acc = Color(0, 0, 0);
-
-	if (a_Depth > TRACE_DEPTH) return 0;
-	// trace primary ray
-	a_Dist = 1000000.0f;
-	Eigen::Vector3f pi;
-	Primitive* prim = 0;
-	int result;
-	// find the nearest intersection
-	for (int s = 0; s < my_scene->get_primitives_number() ; s++) {
-
-		Primitive* pr = my_scene->get_primitive(s);
-		int res;
-		if (res = pr->intersect(a_Ray, a_Dist)) {
-			prim = pr;
-			result = res; // 0 = miss, 1 = hit, -1 = hit from inside primitive
-		}
-	}
-	// no hit, terminate ray
-	if (!prim) return acc;
-	// handle intersection
-	if (prim->is_light()) {
-		// we hit a light, stop tracing
-		acc = Color(1, 1, 1);
-	} else {
-		// determine color at point of intersection
-		pi = a_Ray.get_origin() + a_Ray.get_direction() * a_Dist;
-		// trace lights
-		for (int l = 0; l < my_scene->get_primitives_number(); l++) {
-			Primitive* p = my_scene->get_primitive(l);
-			if (p->is_light()) {
-				Primitive* light = p;
-				// calculate diffuse shading
-				Eigen::Vector3f L = ((Sphere*)light)->get_centre() - pi;
-				L.normalize();
-				Eigen::Vector3f N = prim->get_normal(pi);
-				Material local_material = prim->get_material();
-				if (local_material.get_diffuse() > 0) {
-					float dot = N.dot(L);
-					if (dot > 0) {
-						float diff = dot * local_material.get_diffuse();
-						// add diffuse component to ray color
-						acc = calculateDiffuse(acc, diff, prim->get_material().get_color(), light->get_material().get_color());
-					}
-				}
-			}
-		}
-	}
-	// return pointer to primitive hit by primary ray
-	return acc;
-}
-
-void JBEngine::render(void* target, void* mailbox, const void* parameters) {
-	//// screen plane in world space coordinates
-	//m_WX1 = -4, m_WX2 = 4, m_WY1 = m_SY = 3, m_WY2 = -3;
-	//// calculate deltas for interpolation
-	//m_DX = (m_WX2 - m_WX1) / width;
-	//m_DY = (m_WY2 - m_WY1) / height;
-	//m_SY += 20 * m_DY;
-	//// allocate space to store pointers to primitives for previous line
-	//m_LastRow = new Primitive*[width];
-	//memset(m_LastRow, 0, width * 4);
-
-	pixel* image = (pixel*)target;
-
-	MailBox* my_mailbox = (MailBox *)mailbox;
-	const Parameters* my_parameters = (const Parameters *)parameters;
-
-	const int thread_no = my_parameters->threadNumber;
-	const int total_threads = my_parameters->totalThreads;
-	const int band = my_parameters->height / total_threads;
-	const int startY = thread_no * band;
-	const int endY = thread_no == total_threads - 1 ? height : thread_no * band + band;
-
-	//// set firts line to draw to
-	//int m_CurrLine = 20;
-	//// set pixel buffer address of first pixel
-	//int m_PPos = 20 * width;
-
-	my_mailbox->work_started(thread_no);
-	pixel* currentPixel = image + (startY * width);
-
-	// The Origin point
-	Eigen::Vector3f origin(0, 0, -5);
-
-	// Trace rays
-	bool quit = false;
-	for (int y = startY; !quit && y < endY; ++y) {
-		for (int x = 0; !quit && x < width; ++x, ++currentPixel) {
-
-			// fire primary ray
-			Eigen::Vector3f dir = Eigen::Vector3f(x, y, 0) - origin;
-			dir.normalize();
-			Ray r(origin, dir);
-			float dist;
-			color acc = Raytrace(r,  1, 1.0f, dist);
-
-			*currentPixel = acc;
-			quit = my_mailbox->to_children_quit;
-		}
-	}
-
-	my_mailbox->work_done(thread_no);
-}
-
-}
--- SoftTracer/Renderers/Raytracer.h
+++ SoftTracer/Renderers/Raytracer.h
@@ -263,42 +263,7 @@ namespace JBRaytracer {
 		void render(void* target, void* mailbox, const void* parameters);
 	protected:
 		// renderer data
-		float m_WX1, m_WY1, m_WX2, m_WY2, m_DX, m_DY, m_SX, m_SY;
-
-		int m_Width, m_Height, m_CurrLine, m_PPos;
 	};
 
 
 }; // namespace JBRaytracer
-
-
-namespace Attempt1 {
-
-// Ported from Jacco Bikker 2004 Tutorial engine
-class JBEngine : public CallableAlgorithm {
-protected:
-	// renderer data
-	Scene* my_scene;
-
-	// float m_WX1, m_WY1, m_WX2, m_WY2, m_DX, m_DY, m_SX, m_SY;
-	int width, height;
-
-	//Primitive** m_LastRow;
-
-public:
-	JBEngine();
-
-	~JBEngine();
-
-	void initialize_scene(const void* parameters);
-
-	void render(void* target, void* mailbox, const void* parameters);
-
-private:
-	color Raytrace(Ray& a_Ray, int a_Depth, float a_RIndex, float& a_Dist);
-	
-//	bool Render();
-
-};
-
-}
