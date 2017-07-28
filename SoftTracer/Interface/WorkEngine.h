0944ebad98c3fa085f0516aa584a56e089219722
Davide -limaCAT- Inglima <limacat@live.it>
2017-07-28 21:10:37 +0200

    Removed the old WorkEngine interface

--- SoftTracer/Interface/WorkEngine.h
+++ SoftTracer/Interface/WorkEngine.h
@@ -1,22 +1,4 @@
 #pragma once
-//class WorkEngine {
-//public:
-//	WorkEngine() {
-//	}
-//
-//	virtual ~WorkEngine() {
-//	}
-//
-//	virtual void initialize_scene(const void* parameters) = 0;
-//
-//	/**
-//	 * The render() function willfully does not specify neither the type of target, nor mailbox nor parameters,
-//	 * to allow implementors to decide on classes they need.
-//	 */
-//	virtual void render(void* target, void* mailbox, const void* parameters) = 0;
-//
-//};
-
 class WorkEngine {
 public:
 	WorkEngine() {
